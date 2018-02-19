7f4daf6983ee1c62485bcc45b45a0e400004576c
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-19 19:51:25 +0100

    add article model added show and create method to controller

--- features/step_definitions/create_article.rb
+++ features/step_definitions/create_article.rb
@@ -6,8 +6,8 @@ When("I click {string} link") do |string|
   page.click_link('new article')
 end
 
-When("I fill in {string} with {string}") do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I fill in {string} with {string}") do |field, input|
+  fill_in(field, :with => input)
 end
 
 When("I click {string} button") do |string|
