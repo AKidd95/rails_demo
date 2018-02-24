a739d394158027c03bda6f304e747396bbaa1c32
Isabelle <isabellelidberg@live.se>
2018-02-24 19:39:25 +0100

    add comment message, add flash keys

--- features/step_definitions/article_comment_steps.rb
+++ features/step_definitions/article_comment_steps.rb
@@ -11,7 +11,7 @@ When("I click {string}") do |string|
 end
 
 Then("I should be on the {string} page") do |article_title|
-  article = Articles.find_by(title: article_title)
+  article = Article.find_by(title: article_title)
   expect(page.current_path).to eq article_path(article)
 end
 
