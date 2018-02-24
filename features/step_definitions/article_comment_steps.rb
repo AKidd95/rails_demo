ae8ea0a426500a6f5bb182138372db7d7f6bebc6
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-24 18:10:56 +0100

    add steps tests

--- features/step_definitions/article_comment_steps.rb
+++ features/step_definitions/article_comment_steps.rb
@@ -1,5 +1,5 @@
-When("I click on {string}") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click on {string}") do |link_text|
+  click_link_or_button link_text # Write code here that turns the phrase above into concrete actions
 end
 
 When("I fill in {string} with {string}") do |string, string2|
@@ -10,8 +10,9 @@ When("I click {string}") do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then("I should be on the {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+Then("I should be on the {string} page") do |article_title|
+  article = Articles.find_by(title: article_title)
+  expect(page.current_path).to eq article_path(article)
 end
 
 Then("i should see {string}") do |string|
