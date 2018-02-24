f975b576e0019af667139164b7b2ef25a6c67179
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-24 18:00:01 +0100

    create feature files and steps

--- features/step_definitions/create_article.rb
+++ features/step_definitions/create_article.rb
@@ -1,19 +1,19 @@
-Given("I visit the {string} page") do |string|
-   visit root_path
-end
-
-When("I click {string} link") do |string|
-  page.click_link('new article')
-end
-
-When("I fill in {string} with {string}") do |title, content|
-  fill_in(title, :with => content)
-end
-
-When("I click {string} button") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then("I should be on {string} page") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
-end
+# Given("I visit the {string} page") do |string|
+#    visit root_path
+# end
+#
+# When("I click {string} link") do |string|
+#   page.click_link('new article')
+# end
+#
+# When("I fill in {string} with {string}") do |title, content|
+#   fill_in(title, :with => content)
+# end
+#
+# When("I click {string} button") do |string|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then("I should be on {string} page") do |string|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
---
+++ features/user_can_comment_on_article.feature
@@ -0,0 +1,20 @@
+Feature:
+  As a Blogger
+  In order to talk about my thoughts on an article
+  I would like to be able to comment
+
+    Background:
+      Given the following articles exists
+        | title                | content                          |
+        | A breaking news item | Some really breaking action      |
+        | Learn Rails 5        | Build awesome rails applications |
+
+    Scenario: User adds comment to an articles
+      When I visit the site
+      And I click on "Learn Rails 5"
+      And I fill in "Comment" with "Really bad read"
+      And I fill in "Email" with "aiden.jubelin@gmail.com"
+      And I click "Create Comment"
+      Then I should be on the "Learn Rails 5" page
+      And I should see "Really bad read"
+      And i should see "aiden.jubelin@gmail.com"
