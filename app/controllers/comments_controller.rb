4de5c47358163dfad4af5baeb5d1e00057e4fa59
Isabelle <isabellelidberg@live.se>
2018-02-24 19:11:01 +0100

    sanitize params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.create(comment_params)
   end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:email, :body)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'comments/create'
 
   get 'articles/index'
 
